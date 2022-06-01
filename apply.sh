137aadb4478f9c3c781f08dae669e0a52af8de60
Andreas Kongstad <90042173+Akongstad@users.noreply.github.com>
2022-06-01 10:06:45 +0200

    Update apply.sh
    Add rollout restart

--- apply.sh
+++ apply.sh
@@ -6,3 +6,5 @@ cd manifests
 ls | grep yaml | xargs -I pattern kubectl apply -f pattern
 
 cd ..
+kubectl rollout restart deployment/backend-deployment
+kubectl rollout restart deployment/frontend-deployment 
